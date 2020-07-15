8b813721790bd660dfcaa3712b5af631b7d9dce3
Shashwat <reshmeshashwat@gmail.com>
2020-07-15 22:26:00 +0530

    Added 2 Paddles

--- Pong.py
+++ Pong.py
@@ -7,8 +7,24 @@ wn.setup(width = 800 , height = 600) #size of the window
 wn.tracer (0) #stops the window from updating so we have to manually update it (makes game faster)
 
 
+# Paddle A
+paddle_a = turtle.Turtle()#module name and the class name
+paddle_a.speed(0) #speeds down everything
+paddle_a.shape("square") #shape
+paddle_a.color("white") #colour of the object
+paddle_a.shapesize(stretch_wid=5,stretch_len=1) # size of the object
+paddle_a.penup() #so that lines are not drawn
+paddle_a.goto(-350, 0)#specification of the sstarting point
 
 
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 # Main game loop
 while True:
