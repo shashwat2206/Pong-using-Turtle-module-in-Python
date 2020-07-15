b25af9ee3da20e7854a0bae31711c4e3da5bd532
Shashwat <reshmeshashwat@gmail.com>
2020-07-16 01:50:10 +0530

    Added scores and players

--- Pong.py
+++ Pong.py
@@ -1,21 +1,24 @@
-import turtle 
-from turtle import *
-bgcolor("black")
-wn = turtle.Screen() #opens a window
-wn.title("Pong by Shashwat") #title of the project
-wn.setup(width = 800 , height = 600) #size of the window
-wn.tracer (0) #stops the window from updating so we have to manually update it (makes game faster)
+import turtle
+import os
 
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
 
 # Paddle A
-paddle_a = turtle.Turtle()#module name and the class name
-paddle_a.speed(0) #speeds down everything
-paddle_a.shape("square") #shape
-paddle_a.color("white") #colour of the object
-paddle_a.shapesize(stretch_wid=5,stretch_len=1) # size of the object
-paddle_a.penup() #so that lines are not drawn
-paddle_a.goto(-350, 0)#specification of the sstarting point
-
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
 # Paddle B
 paddle_b = turtle.Turtle()
@@ -25,60 +28,97 @@ paddle_b.color("white")
 paddle_b.shapesize(stretch_wid=5,stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
-#Ball
+
+# Ball
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
-ball.shapesize(stretch_wid=1 ,stretch_len=1)
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 0.25
-ball.dy = 0.25
-#Function
+ball.dx = 0.3
+ball.dy = 0.3
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+# Functions
 def paddle_a_up():
     y = paddle_a.ycor()
-    y +=20
+    y += 20
     paddle_a.sety(y)
 
 def paddle_a_down():
     y = paddle_a.ycor()
-    y -=20
+    y -= 20
     paddle_a.sety(y)
 
 def paddle_b_up():
     y = paddle_b.ycor()
-    y +=20
+    y += 20
     paddle_b.sety(y)
 
 def paddle_b_down():
     y = paddle_b.ycor()
-    y -=20
+    y -= 20
     paddle_b.sety(y)
 
-#Keyboard Binding
-wn.listen() #listens to the key board input
-wn.onkeypress(paddle_a_up, "w")  #to move pad up
-wn.onkeypress(paddle_a_down, "s")  #----""---- down
-wn.onkeypress(paddle_b_up, "Up")  
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
+
 # Main game loop
 while True:
-    wn.update () #every time the loop runs it updates the screen
-    # Moving the Ball
-    ball.setx(ball.xcor() + ball.dx )
-    ball.sety(ball.ycor() + ball.dy )
+    wn.update()
+    
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+
+    # Top and bottom
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
-
+        
+    
     elif ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
-    if ball.xcor() > 390 :
-        ball.goto(0,0)
-        ball.dx *= -1
-    elif ball.xcor() < -390 :
-        ball.goto(0,0)
+        
+
+    # Left and right
+    if ball.xcor() > 350:
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
         ball.dx *= -1
 
+    elif ball.xcor() < -350:
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    # Paddle and ball collisions
+    if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+        ball.dx *= -1 
+        
+    
+    elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+        ball.dx *= -1
+        
+    
